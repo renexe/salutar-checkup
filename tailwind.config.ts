a7092730a88d639fb049426c8ef5815de2c4d56d
Renê Lima <rene.exe@gmail.com>
2023-11-05 15:48:32 -0300

    <setup>[theme]: Start to config the theme system

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,14 @@
-import type { Config } from 'tailwindcss'
+import { theme } from "./src/themes/theme";
 
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
+  theme: theme,
+  plugins: [require("tailwindcss-animate")],
 }
-export default config
