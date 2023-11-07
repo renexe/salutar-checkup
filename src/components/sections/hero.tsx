4336fefa6fa3cd0dad6830add8430184960812e9
Renê Lima <rene.exe@gmail.com>
2023-11-06 21:40:51 -0300

    <feat>[sections]: Start to craft the Hero

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -2,8 +2,8 @@ import { Typography } from "../ui/typography"
 
 const Hero = () => {
   return (
-    <section className="relative bg-cover bg-[url(/assets/images/background_hero.jpg)] w-full py-16 px-8 grid items-center grid-cols-[repeat(auto-fit_minmax(240px,_1fr))] min-h-screen">
-      <Typography variant="h1">O que faz você melhor, faz a gente melhor</Typography>
+    <section className="relative bg-cover bg-[url(/assets/images/hero-1.png)] w-full py-16 px-8 grid items-center grid-cols-[repeat(auto-fit_minmax(240px,_1fr))] min-h-screen">
+      <Typography variant="special-title">O que faz você melhor, faz a gente melhor</Typography>
       <article>
         <p>Explore local breweries with just one click and stirred by starlight across the centuries light years great turbulent clouds circumnavigated paroxysm of global death.</p>
         <a href="#breweries">Browse Breweries</a>
