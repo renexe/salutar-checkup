4f93f920fb93768b02160f217ddf90030c908ddf
Renê Lima <rene.exe@gmail.com>
2023-11-07 21:37:47 -0300

    <style>[header]: Responsiveness of main-nav

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -11,22 +11,18 @@ interface MainNavProps {
 
 export function MainNav({ items }: MainNavProps) {
   return (
-    <div className="flex gap-6 md:gap-20 h-full">
+    <div className="flex md:gap-6 lg:gap-20 h-full">
       <Link href="/" className="flex items-center space-x-2">
         <Typography variant={"h4"} className="inline-block font-bold text-primary-foreground">{siteConfig.name}</Typography>
       </Link>
       {items?.length ? (
-        <nav className="flex gap-2 items-center">
+        <nav className="md:flex gap-2 items-center hidden">
           {items?.map(
             (item, index) =>
               item.href && (
                 <Link
                   key={index}
                   href={item.href}
-                  // className={cn(
-                  //   "flex items-center text-base font-medium text-primary-foreground hover:text transition-all duration-200",
-                  //   item.disabled && "cursor-not-allowed opacity-80"
-                  // )}
                   className="bg-primary text-primary-foreground px-3 py-0 hover:py-5 hover:bg-accent/90 transition-all duration-400"
                 >
                   {item.title}
