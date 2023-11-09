499bb894fc88e17a3c2e2d88886fe29200fb914a
Renê Lima <rene.exe@gmail.com>
2023-11-09 13:43:34 -0300

    <feat>[ui]: Craft header mutable with scroll

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -4,16 +4,18 @@ import Link from "next/link"
 import { NavItem } from "@/types/nav"
 import { siteConfig } from "@/config/site"
 import { Typography } from "./ui/typography"
+import { cn } from "@/lib/utils"
 
 interface MainNavProps {
-  items?: NavItem[]
+  items?: NavItem[],
+  isNavFilled: boolean
 }
 
-export function MainNav({ items }: MainNavProps) {
+export function MainNav({ items, isNavFilled }: MainNavProps) {
   return (
     <div className="flex md:gap-6 lg:gap-20 h-full">
       <Link href="/" className="flex items-center space-x-2">
-        <Typography variant={"h4"} className="inline-block font-bold">{siteConfig.name}</Typography>
+        <Typography variant={"h4"} color={isNavFilled ? "white" : "foreground"} className={"inline-block font-bold"}>{siteConfig.name}</Typography>
       </Link>
       {items?.length ? (
         <nav className="md:flex gap-2 items-center hidden">
@@ -23,7 +25,7 @@ export function MainNav({ items }: MainNavProps) {
                 <Link
                   key={index}
                   href={item.href}
-                  className="leading-8 overflow-hidden px-3 relative font-bold before:content-[''] before:absolute before:bottom-0 before:left-0 before:w-full before:h-1 before:bg-foreground before:transition-all before:duration-300 before:z-10 hover:before:translate-x-0 before:-translate-x-full"
+                  className={cn(isNavFilled ? "text-white before:bg-white" : "text-foreground before:bg-foreground", "leading-8 overflow-hidden px-3 relative font-bold before:content-[''] before:absolute before:bottom-0 before:left-0 before:w-full before:h-1 before:transition-all before:duration-300 before:z-10 hover:before:translate-x-0 before:-translate-x-full")}
                 >
                   {item.title}
                 </Link>
--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,16 +1,32 @@
+'use client'
 import { siteConfig } from "@/config/site"
 import { Button } from "@/components/ui/button"
 import { MainNav } from "@/components/main-nav"
+import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
 
 
 export function SiteHeader() {
+
+  const [isNavFilled, setIsNavFilled] = useState<boolean>(false);
+  const listenScrollEvent = () => {
+    window.scrollY > 10 ? setIsNavFilled(true) : setIsNavFilled(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", listenScrollEvent);
+    return () => {
+      window.removeEventListener("scroll", listenScrollEvent);
+    };
+  }, []);
+
   return (
-    <header className="fixed top-0 z-40 w-full bg-transparent py-6">
+    <header className={cn(isNavFilled ? "bg-foreground" : "bg-transparent", "fixed top-0 z-40 w-full py-6 transition-all duration-300")}>
       <div className="container flex items-center space-x-4 sm:justify-between sm:space-x-0">
-        <MainNav items={siteConfig.mainNav} />
+        <MainNav items={siteConfig.mainNav} isNavFilled={isNavFilled} />
         <div className="hidden md:flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
-            <Button size="lg">Portal do paciente</Button>
+            <Button size="lg" variant={isNavFilled ? "default-foreground" : "default"} >Portal do paciente</Button>
           </nav>
         </div>
       </div>
