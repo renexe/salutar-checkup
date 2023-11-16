cfc9009e11b02c2fa6177b25471dc9777511c55c
Renê Lima <rene.exe@gmail.com>
2023-11-16 20:36:33 -0300

    <feat> Crafting the tow cols section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import SectionFeatures from '@/components/sections/features'
 import Hero from '@/components/sections/hero'
 import SectionMarkers from '@/components/sections/icon-markers'
 import SectionPrograms from '@/components/sections/programs'
+import SectionTwoColumns from '@/components/sections/two-columns'
 import Image from 'next/image'
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
       <SectionFeatures />
       <SectionMarkers />
       <SectionPrograms />
+      <SectionTwoColumns />
     </main>
   )
 }
---
+++ src/components/sections/two-columns.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image"
+import { Typography } from "../ui/typography"
+
+export default function SectionTwoColumns() {
+
+  return (
+    <section className="relative w-full mb-32">
+      <div className="container mx-auto px-4">
+        <div className="grid grid-cols-2 gap-8">
+          <div className="col-span-2 md:col-span-1">
+            <Typography variant="h2" color="foreground" className="mb-8">
+              Estamos presentes em mais de 20 cidades no sudoeste da Bahia
+            </Typography>
+            <Typography variant="h6" color="foreground" className="max-w-[33vw]">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu euismod libero, sed lobortis libero. Nam a eleifend ligula. Phasellus venenatis est leo. Nullam porta magna sit amet semper pellentesque. Suspendisse gravida mauris non ex cursus, vel varius est congue. Integer fermentum vehicula purus at venenatis. Nam tristique a odio vel pretium. Mauris blandit tristique finibus. Suspendisse potenti. Vivamus vitae arcu enim. Morbi quis rutrum ipsum. Ut feugiat mi dictum purus varius, porttitor congue erat cursus. Ut pellentesque pretium nulla.
+            </Typography>
+          </div>
+          <div className="col-span-2 md:col-span-1 relative min-h-[60vh]">
+            <Image src="/assets/images/mapa.png" alt="Um grupo de médicos" width={576} height={620} />
+          </div>
+        </div>
+        <div className="grid grid-cols-2 gap-8 mt-24">
+          <div className="col-span-2 md:col-span-1 relative min-h-[60vh]">
+            <Image src="/assets/images/medicos.jpeg" alt="Um grupo de médicos" fill className="object-cover rounded-lg" />
+          </div>
+          <div className="col-span-2 md:col-span-1">
+            <Typography variant="h2" color="foreground" className="mb-8 max-w-[40vw]">
+              Contamos com um time de médicos especialistas de diversas áreas
+            </Typography>
+            <Typography variant="h6" color="foreground" className="max-w-[33vw]">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu euismod libero, sed lobortis libero. Nam a eleifend ligula. Phasellus venenatis est leo. Nullam porta magna sit amet semper pellentesque. Suspendisse gravida mauris non ex cursus, vel varius est congue. Integer fermentum vehicula purus at venenatis. Nam tristique a odio vel pretium. Mauris blandit tristique finibus. Suspendisse potenti. Vivamus vitae arcu enim. Morbi quis rutrum ipsum. Ut feugiat mi dictum purus varius, porttitor congue erat cursus. Ut pellentesque pretium nulla.
+            </Typography>
+          </div>
+        </div>
+      </div>
+
+    </section>
+  )
+}
+
+SectionTwoColumns.displayName = 'SectionPrograms'
