25989125ac267a1be23075be14a62b429791187a
Renê Lima <rene.exe@gmail.com>
2023-11-17 23:16:10 -0300

    <fix> Mapping the mocked faqs

--- src/components/sections/faqs.tsx
+++ src/components/sections/faqs.tsx
@@ -1,3 +1,4 @@
+'use client'
 import {
   Accordion,
   AccordionContent,
@@ -8,38 +9,22 @@ import { Typography } from "../ui/typography"
 
 
 export default function SectionFaqs() {
-  const programs = [
+  const faqs = [
     {
-      title: 'Program 1',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam id tincidunt dapibus adipiscing elit',
+      question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam id tincidunt dapibus adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
     },
     {
-      title: 'Program 2',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam id tincidunt dapibus adipiscing elit',
+      question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam id tincidunt dapibus adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
     },
     {
-      title: 'Program 3',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam id tincidunt dapibus adipiscing elit',
+      question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam id tincidunt dapibus adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
     },
     {
-      title: 'Program 4',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam id tincidunt dapibus adipiscing elit',
-    },
-    {
-      title: 'Program 1',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam id tincidunt dapibus adipiscing elit',
-    },
-    {
-      title: 'Program 2',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam id tincidunt dapibus adipiscing elit',
-    },
-    {
-      title: 'Program 3',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam id tincidunt dapibus adipiscing elit',
-    },
-    {
-      title: 'Program 4',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam id tincidunt dapibus adipiscing elit',
+      question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam id tincidunt dapibus adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
     },
   ]
 
@@ -48,39 +33,18 @@ export default function SectionFaqs() {
       <Typography variant="h1" color="foreground" className="text-center mb-12">Perguntas frequentes</Typography>
       <div className="container px-4 md:px-40 lg:px-60">
         <Accordion type="single" collapsible className="w-full">
-          <AccordionItem value="item-1">
-            <AccordionTrigger>
-              <Typography variant="h4" color="foreground">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Typography>
-            </AccordionTrigger>
-            <AccordionContent>
-            <Typography variant="h5" color="foreground">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam id tincidunt dapibus adipiscing elit
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam id tincidunt dapibus adipiscing elit
-            </Typography>
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-2">
-            <AccordionTrigger>
-              <Typography variant="h4" color="foreground">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Typography>
-            </AccordionTrigger>
-            <AccordionContent>
-            <Typography variant="h5" color="foreground">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam id tincidunt dapibus adipiscing elit
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam id tincidunt dapibus adipiscing elit
-            </Typography>
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-3">
-            <AccordionTrigger>
-              <Typography variant="h4" color="foreground">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Typography>
-            </AccordionTrigger>
-            <AccordionContent>
-            <Typography variant="h5" color="foreground">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam id tincidunt dapibus adipiscing elit
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam id tincidunt dapibus adipiscing elit
-            </Typography>
-            </AccordionContent>
-          </AccordionItem>
+          {faqs.map((faq, index) => (
+            <AccordionItem value={`item-${index}`} className="border rounded-lg border-foreground px-4 border-b-0 last-of-type:border-b">
+              <AccordionTrigger>
+                <Typography variant="h4" color="foreground">{faq.question}</Typography>
+              </AccordionTrigger>
+              <AccordionContent>
+                <Typography variant="h5" color="foreground">
+                  {faq.answer}
+                </Typography>
+              </AccordionContent>
+            </AccordionItem>
+          ))}
         </Accordion>
       </div>
     </section>
